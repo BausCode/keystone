327700a12c568ecbc3ad4d68b9bc5a7bac4aafbb
James Cunningham <james@bauscode.com>
2016-06-17 11:34:00 -0600

    add(BAUS) support for base path from editor config

--- admin/api/s3.js
+++ admin/api/s3.js
@@ -12,9 +12,11 @@ exports = module.exports = {
 		if(req.files && req.files.file){
 
 			var s3Config = keystone.get('s3 config');
+			var editorOptions = keystone.get('wysiwyg additional options');
 
 			var file = req.files.file,
-				path = s3Config.s3path ? s3Config.s3path + '/' : '';
+				path = (s3Config.s3path ? s3Config.s3path + '/' : '') + 
+					(editorOptions.images_upload_base_path ? editorOptions.images_upload_base_path + '/' : '');
 
 			var headers = Types.S3File.prototype.generateHeaders.call({ s3config: s3Config, options: {} }, null, file);
 
@@ -30,7 +32,7 @@ exports = module.exports = {
 						if (s3Response.statusCode !== 200) {
 							return res.send({ error: { message:'Amazon returned Http Code: ' + s3Response.statusCode } });
 						} else {
-							return res.send({ image: { url: 'https://s3.amazonaws.com/' + s3Config.bucket + '/' + file.name } });
+							return res.send({ image: { url: 'https://s3.amazonaws.com/' + s3Config.bucket + '/' + path + file.name } });
 						}
 					}
 				};
